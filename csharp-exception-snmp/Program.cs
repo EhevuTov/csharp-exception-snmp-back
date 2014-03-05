8166228894363a2d3043569ba9e620850d40cfe8
James Gosnell <jamesgosnell@gmail.com>
2014-03-05 02:25:13 -0600

    ADDED SNMP trap send

--- csharp-exception-snmp/Program.cs
+++ csharp-exception-snmp/Program.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 using System.Threading.Tasks;
+
+using Lextm.SharpSnmpLib;
+using Lextm.SharpSnmpLib.Messaging;
 
 namespace csharp_exception_snmp
 {
@@ -10,7 +14,15 @@ namespace csharp_exception_snmp
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello, World!");
+            System.Console.WriteLine("Hello, SNMP!");
+            IPAddress address = args.Length == 1 ? IPAddress.Parse(args[0]) : IPAddress.Loopback;
+            Messenger.SendTrapV2(0, VersionCode.V2, new IPEndPoint(address, 162),
+                new OctetString("public"),
+                new ObjectIdentifier(new uint[] { 1, 3, 6 }),
+                0,
+                new List<Variable>());
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
         }
     }
 }
