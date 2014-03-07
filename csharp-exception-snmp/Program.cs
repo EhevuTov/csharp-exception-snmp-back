56ef0f19b04f6405bb22b70ab436e6ef2de20d12
James Gosnell <jamesgosnell@gmail.com>
2014-03-07 05:24:20 -0600

    ADDED exception test

--- csharp-exception-snmp/Program.cs
+++ csharp-exception-snmp/Program.cs
@@ -12,17 +12,43 @@ namespace csharp_exception_snmp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void testFunc()
         {
-            System.Console.WriteLine("Hello, SNMP!");
-            IPAddress address = args.Length == 1 ? IPAddress.Parse(args[0]) : IPAddress.Loopback;
+            int i = 0;
+            if (i == 0)
+            {
+               // throw new System.ArgumentException("Parameter cannot be 0", "original");
+                throw new System.Exception("Something real bad happen!");
+            }
+
+        }
+
+        static void sendTrap(IPAddress address)
+        {
+
             Messenger.SendTrapV2(0, VersionCode.V2, new IPEndPoint(address, 162),
                 new OctetString("public"),
                 new ObjectIdentifier(new uint[] { 1, 3, 6 }),
                 0,
                 new List<Variable>());
+        }
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, SNMP!");
+            IPAddress address = args.Length == 1 ? IPAddress.Parse(args[0]) : IPAddress.Loopback;
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);
+            try
+            {
+                testFunc();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                sendTrap(address);
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey(true);
+            }
         }
     }
 }
